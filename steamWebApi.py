af7c2e4f6c3b65822f5e72cb95a7083d73c0730f
zigzag1001 <72932714+zigzag1001@users.noreply.github.com>
2021-06-02 17:11:16 -0400

    reworked a bit

--- steamWebApi.py
+++ steamWebApi.py
@@ -1,38 +1,106 @@
 import requests
 import json
 import os
+import time
 
+# file path for steam id jsons
+filePath = "./people/"
+key = input("paste in key:\n")
+
+# gets friends of input steam id, returns dict
 def getFriendList(steamid):
-	if os.path.isfile("TEMPfriends.json"):
-		os.remove("TEMPfriends.json")
 
-	filePath = "./people/"
-	data = {'key': '5C1C9CF6CAD6943D87C44DC6B3957F94',
+  numOfFriends = 0
+  
+  new = {}
+  
+  # data required for request
+  data = {'key': str(key),
 	'steamid': str(steamid),
 	'relationship': 'friend'}
+	
+  # url which request is sent to
+  url = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="+data['key']+"&steamid="+data['steamid']+"&relationship=friend"
+  
+  # send request and save the data to req
+  req = requests.get(url, data = data).text
 
-	url = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=5C1C9CF6CAD6943D87C44DC6B3957F94&steamid="+data['steamid']+"&relationship=friend"
+  # if profile is private return empty dict
+  if req == '{}':
+	  return {}
 
-	req = requests.get(url, data = data).text
-	if req == '{}':
-		print("private profile (api request returned nothing)")
-		return
+  # loading requested info as json
+  another = json.loads(req)
+	  
+  # filtering out everything else except steam id
+  for friend in another['friendslist']['friends']:
+	  new[friend['steamid']] = ''
+	  numOfFriends += 1
 
-	with open('TEMPfriends.json', "w") as friends:
-		friends.write(req)
+  # returns filtered dict
+  return new
+	
+# writes friends to new file
+def writeToFile(full, steamid):
+  
+	new = {}
+	
+	steamidFileName = filePath + steamid + ".json"
+	if os.path.isfile(steamidFileName):
+	  os.remove(steamidFileName)
 
-	with open("TEMPfriends.json") as f:
-		full = json.load(f)
+	with open(steamidFileName, "w") as file:
+		json.dump(full, file, indent = 2)
 
-	with open(filePath + data['steamid'] + ".json", "w") as file:
-		for friend in full['friendslist']['friends']:
-			# print(friend["steamid"])
-			file.write(friend["steamid"] + ",\n")
+# loads friends list and gets friends of every friend of initial user
+def getFoF(fileName):
 
-	input("press enter to continue")
+  # adds .json to end of steamid
+  if not ".json" in fileName:
+    fileName += ".json"
+  
+  # opends file and gets friends of every friend of initial user
+  with open(filePath + fileName) as f:
+    ids = json.load(f)
+    for id in ids:
+      ids[id] = getFriendList(id)
+      # print("===")
+    return ids
 
-	if os.path.isfile("TEMPfriends.json"):
-		os.remove("TEMPfriends.json")
-	print("done")
+# loads friends list and gets friends of friends of every friend of initial user
+def getFoFoF(fileName):
 
-getFriendList("76561198199384013")
+  # adds .json to end of steamid
+  if not ".json" in fileName:
+    fileName += ".json"
+  
+  # opends file and gets friends of friends of every friend of initial user
+  with open(filePath + fileName) as f:
+    ids = json.load(f)
+    for id in ids:
+      for idd in ids[id]:
+        ids[id][idd] = getFriendList(idd)
+      # print("===")
+    return ids
+  
+# steamid
+id = "76561198967207139"
+
+print("started")
+  
+# generates initial json file with friends of input id
+writeToFile(getFriendList(id), id)
+
+# gets friends of friends and dumps into the same json file
+toDump = getFoF(id)
+with open(filePath + id + ".json", "w") as file:
+  json.dump(toDump, file, indent = 2)
+
+startTime = time.time()
+print("started big boi")
+
+toDump = getFoFoF(id)
+with open(filePath + id + ".json", "w") as file:
+  json.dump(toDump, file, indent = 2)
+
+print("last task done in", time.time()-startTime)
